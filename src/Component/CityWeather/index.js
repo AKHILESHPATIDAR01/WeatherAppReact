3e7ba0954f4c92acb53d8014af7feb9b89314ec6
AKHILESHPATIDAR01 <akhilesh.forsignin@gmail.com>
2021-02-12 18:27:28 +0530

    first commit

--- src/Component/CityWeather/index.js
+++ src/Component/CityWeather/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css';
 
 const CityWeather = (weather) => {
     // console.log(weather);
@@ -7,9 +8,13 @@ const CityWeather = (weather) => {
     // const {data} = cityWeather;
     return (
         <div>
-            <div>
-                <div> City Name =  {weather.weather.name} </div>
-                Wind Speed = {weather.weather.wind.speed} 
+            <div className="box">
+                <div> <span>City Name </span> =  {weather.weather.name} </div>
+                <div> <span>Wind Speed </span> = {weather.weather.wind.speed}  </div>
+                <div> <span>humidity </span> = {weather.weather.main.humidity}</div>
+                <div> <span>Temprature </span> = { Math.floor(weather.weather.main.temp -273.15)} <sup>o</sup> C </div>
+                <div> <span>Max-Temprature </span> = { Math.floor(weather.weather.main.temp_max -273.15)} <sup>o</sup> C </div>
+                <div> <span>Min-Temprature </span> = { Math.floor(weather.weather.main.temp_min -273.15)} <sup>o</sup> C </div>
             </div>
         </div>
     );
