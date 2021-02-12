e64017ed25e8b9917df6219e257f5e3fe08c44a9
AKHILESHPATIDAR01 <akhilesh.forsignin@gmail.com>
2021-02-12 16:46:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import City from './Component/City';
+import CityWeather from './Component/CityWeather';
+
+
 
 function App() {
+  const [cityName , setCityName] = React.useState("");
+
+  const fetchCityWeather = () =>{
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4deb3d5b5a9ba1475a3e91881ff30ce9`).then((res) => res.json()).then((result)=> {
+      console.log("Result is ",result);
+    }) ;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <City cityName={cityName} setCityName={setCityName} fetchCityWeather={fetchCityWeather} />
+        <CityWeather />
+    </>
   );
 }
 
