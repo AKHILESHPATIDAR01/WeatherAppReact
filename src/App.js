c707357b1415c0513aefe2360c2d872e1637f88c
AKHILESHPATIDAR01 <71465599+AKHILESHPATIDAR01@users.noreply.github.com>
2021-02-18 21:07:12 +0530

    Update App.js
    Update location https error

--- src/App.js
+++ src/App.js
@@ -11,22 +11,37 @@ function App() {
   
   const [cityName , setCityName] = React.useState("");
   const [cityWeather , setCityWeather] = React.useState([]);
-  const fetchCityWeather = () =>{
-    console.log("I am from fetchCityWeather ");
+ const fetchCityWeather = () =>{
+    // console.log("I am from fetchCityWeather ");
 
+    if (window.location.protocol === 'http:') {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4deb3d5b5a9ba1475a3e91881ff30ce9`)
     .then((res) => { return res.json() })
     .then((result)=> {
-      console.log("Result is ",result);
+      // console.log("Result is ",result);
       setCityWeather( {
           data : result
         }
         );
-    }
+      }
     
     ) ;
+  }
+  else{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4deb3d5b5a9ba1475a3e91881ff30ce9`)
+    .then((res) => { return res.json() })
+    .then((result)=> {
+      // console.log("Result is ",result);
+      setCityWeather( {
+          data : result
+        }
+        );
+      }
     
-  };
+    ) ;
+  }
+    
+  }; 
   return (
     <>
         <City cityName={cityName} setCityName={setCityName} fetchCityWeather={fetchCityWeather} />
